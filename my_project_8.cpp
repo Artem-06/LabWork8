3091d5d45a2cc3d739bfe697e7203d164539b736
Artem Ten <artem.ten0631@gmail.com>
2024-10-29 23:17:42 +0200

    test

---
+++ my_project_8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+
+	float y, Z, dob = 1;
+
+	cout << "Input y: ";
+	cin >> y;
+
+	for (int k = 1; k <= 10; k++)
+	{
+		dob *= log(k * y);
+	}
+
+	Z = y + dob + 2;
+
+	cout << "Z=" << Z;
+}
